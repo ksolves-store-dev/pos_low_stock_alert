d35f6eb3f4a77a6035fc601e3bfd07862e4a15aa
ashishksolves <ashish.chaubey@ksolves.com>
2019-12-23 11:26:47 +0530

    Updated pos low stock alert

--- ks_pos_low_stock_alert/__manifest__.py
+++ ks_pos_low_stock_alert/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': """Manage the Stock of your POS products by highlighting them in different colors depending on
                 their stock state.""",
     'description': """
-        POS Low Stock Alert v12.0
+        POS Low Stock Alert v13.0
         Manage the Stock of your POS products by highlighting them in different colors depending on
                 their stock state.
         POS
@@ -32,7 +32,7 @@
     'license': 'LGPL-3',
     'category': 'Point Of Sale',
     'support': 'sales@ksolves.com',
-    'version': '1.0.0',
+    'version': '13.0.1.0.0',
     'images': ['static/description/main.jpg'],
     'depends': ['point_of_sale'],
     'data': [
